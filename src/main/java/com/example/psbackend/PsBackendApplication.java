d386c561f6ce4499c4154a34d1f51aee51fdf048
tayssir <tayssir.ghanmi@eniso.u-sousse.tn>
2023-12-25 17:38:34 -0500

    starting

---
+++ src/main/java/com/example/psbackend/PsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.psbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/psbackend/PsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.psbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
